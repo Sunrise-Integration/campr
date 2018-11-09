19b9726607f70a4fdc78b0f0d5a2b62dd5410977
matthew@sunriseintegration.com <matthew@sunriseintegration.com>
2018-11-08 16:16:45 -0800

    Updates

--- src/index.mjs
+++ src/index.mjs
@@ -1,42 +1,22 @@
 import dotenv from "dotenv";
 let config = dotenv.config();
 if (!config.error) config = config.parsed;
-import https from "https";
-import fs from "fs";
+import rp from "request-promise";
+import queries from "./queries";
 
 const API_URL = `https://api.github.com/graphql`;
 
-const createPR = {
-  query: `
-  mutation CreatePullRequest {
-    createPullRequest(input: { baseRefName: "master", title: "api pull request", repositoryId: "MDEwOlJlcG9zaXRvcnkxNTM1NDMxNzg=", headRefName: "develop" }) {
-      pullRequest {
-        title
-        id
-      }
-  }
-}`
-};
+const repoOwner = "Sunrise-Integration";
+const repoName = "tasklauncher";
 
-const mergePR = {
-  query: `
-  mutation MergePullRequest {
-    mergePullRequest(input: { pullRequestId: "{{id}}" }) {
-      pullRequest {
-        title
-        id
-      }
-  }
-}`
-};
-console.log(config.token);
-const postData = JSON.stringify(createPR);
-let mergePostData = JSON.stringify(mergePR);
+const getRepoQuery = queries.getRepository(repoOwner, repoName);
 
-const options = {
-  host: "api.github.com",
-  path: "/graphql",
-  method: "POST",
+let options = {
+  uri: "https://api.github.com/graphql",
+  body: {
+    query: getRepoQuery
+  },
+  json: true,
   headers: {
     "Content-type": "application/json",
     Authorization: `Bearer ${config.token}`,
@@ -44,37 +24,54 @@ const options = {
     Accept: "application/vnd.github.ocelot-preview+json"
   }
 };
-const request = https.request(options, res => {
-  res.setEncoding("utf8");
-  console.log(res.statusCode);
-  res.on("data", chunk => {
-    chunk = JSON.parse(chunk);
-    if (res.statusCode === 200) {
-      const { createPullRequest } = chunk.data;
-      const PRId = createPullRequest.pullRequest.id;
-      mergePostData = mergePostData.replace("{{id}}", PRId);
 
-      const mergeRequest = https.request(options, mergeRes => {
-        console.log(mergeRes.statusCode);
+const main = async () => {
+  let data = await rp.post(options);
 
-        mergeRes.on("data", mergeChunk => {
-          mergeChunk = JSON.parse(mergeChunk);
+  const repositoryId = data.data.repository.id;
+  const createPRQuery = queries.createPullRequest(repositoryId, "UI fixes");
 
-          console.log(mergeChunk);
-        });
-      });
+  const newOptions = JSON.parse(JSON.stringify(options));
+  newOptions.query = createPRQuery;
 
-      mergeRequest.on("error", e => {
-        console.error(`${e.message}`);
-      });
-      mergeRequest.write(mergePostData);
-      mergeRequest.end();
-    }
-  });
-});
+  console.log(newOptions);
 
-request.on("error", e => {
-  console.error(`${e.message}`);
-});
-request.write(postData);
-request.end();
+  let pullRequestData = await rp.post(newOptions);
+};
+
+main();
+
+// const request = https.request(options, res => {
+//   res.setEncoding("utf8");
+//   console.log(res.statusCode);
+//   res.on("data", chunk => {
+//     chunk = JSON.parse(chunk);
+//     if (res.statusCode === 200) {
+//       const { createPullRequest } = chunk.data;
+//       const PRId = createPullRequest.pullRequest.id;
+//       mergePostData = mergePostData.replace("{{id}}", PRId);
+
+//       const mergeRequest = https.request(options, mergeRes => {
+//         console.log(mergeRes.statusCode);
+
+//         mergeRes.on("data", mergeChunk => {
+//           mergeChunk = JSON.parse(mergeChunk);
+
+//           console.log(mergeChunk);
+//         });
+//       });
+
+//       mergeRequest.on("error", e => {
+//         console.error(`${e.message}`);
+//       });
+//       mergeRequest.write(mergePostData);
+//       mergeRequest.end();
+//     }
+//   });
+// });
+
+// request.on("error", e => {
+//   console.error(`${e.message}`);
+// });
+// request.write(postData);
+// request.end();
